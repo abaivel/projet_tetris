be271bbe0a8093ca11eb3ec420e1dd8a6b17158b
Alexandra <103836628+abaivel@users.noreply.github.com>
2022-05-17 14:43:01 +0200

    Update fonctions.h

--- fonctions.h
+++ fonctions.h
@@ -1,7 +1,9 @@
+#define TAILLE 10
+
 typedef struct {
 	char** form;
 	int nb_orient;
-	int size[2];
+	int sizeLC[2];
 }Piece;
 
 void show(char** grille);
@@ -9,5 +11,7 @@ void createPiece(Piece* pieces);
 void saveScore(int score, char pseudo[]);
 void recup_Score();
 char getChar();
+void remove_line(char** grille);
+int completeLigne(char** grille, int line[TAILLE]);
+int collisions(char** grille, Piece piece, int column, int orient);
 void game();
-
